aa55c1c6b287b758d4a8b7889711f060f07ff7f9
S540d <S540d@users.noreply.github.com>
2025-10-18 05:38:00 +0000

    🧪 Testing deployment from 1c855c725272ac22d51310f4cef2563077195a10 1c855c725272ac22d51310f4cef2563077195a10

--- testing/service-worker.js
+++ testing/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_VERSION = '2.1.2';
-const BUILD_DATE = '2025-10-18'; // Cache busting - updated 2025-10-18T05:32:21.044Z
+const BUILD_DATE = '2025-10-18'; // Cache busting - updated 2025-10-18T05:37:58.971Z
 const CACHE_NAME = `eisenhauer-matrix-v${CACHE_VERSION}-${BUILD_DATE}`;
 const urlsToCache = [
   './',
