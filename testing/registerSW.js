9cfed8517bc221fbdf92b922c56b36374ac8414f
S540d <S540d@users.noreply.github.com>
2026-02-04 19:20:24 +0000

    🧪 testing deployment from 2296ea2ae0efe755ebb39514d987281b0da0c5a7 2296ea2ae0efe755ebb39514d987281b0da0c5a7

---
+++ testing/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Eisenhauer/testing/service-worker.js', { scope: '/Eisenhauer/testing/' })})}
